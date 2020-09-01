3d2e935478b010502f67d73eed8ea267efba5a3f
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-01 18:04:31 -0400

    Commit

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ const server = express();
 server.use("/api/posts", postRoutes)
 
 
-
-
 server.use("/", (req, res) => res.send("API up and running!"));
 server.listen(9000, () => console.log("API running on port 9000"));
